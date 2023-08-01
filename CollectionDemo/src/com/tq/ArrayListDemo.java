1ad82d02f3497bd40aab3484a45912fc12761e37
Jayshree-10 <tqjayshreec@gmail.com>
2023-08-01 17:39:27 +0530

    data is added

--- CollectionDemo/src/com/tq/ArrayListDemo.java
+++ CollectionDemo/src/com/tq/ArrayListDemo.java
@@ -1,11 +1,16 @@
 package com.tq;
 
+import java.util.ArrayList;
+
 public class ArrayListDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("hi");
+		ArrayList<Integer> arr=new ArrayList<>();
+		arr.add(1);
+		System.out.println(arr);
 	}
 
 }
